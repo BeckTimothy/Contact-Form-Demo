010807dc1e36619ea8e0039a050dacbe08318a71
Timothy Beck <johndaveleet@gmail.com>
2019-11-12 10:18:52 -0700

    added boilerplating

--- public_html/index.php
+++ public_html/index.php
@@ -3,10 +3,27 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Contact Form</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+
 	<!-- jQuery Form, Additional Methods, Validate -->
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+			  integrity="sha384-vk5WoKIaW/vJyUAd9n/wmopsmNhiy+L2Z+SBxGYnUkunIxVxAv/UtMOhba/xskxh"
+			  crossorigin="anonymous"></script>
+	<script type="text/javascript"
+			  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+	<script type="text/javascript"
+			  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+	<script type="text/javascript"
+			  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+	<!--bootstrap-->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+			  integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+			  crossorigin="anonymous"></script>
 
 	<!-- Your JavaScript Form Validator -->
 	<script src="js/form-validate.js"></script>
@@ -14,7 +31,70 @@
 	<!-- Google reCAPTCHA -->
 	<script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
-<body>
 
+<body>
+<div class="col-4">
+	<form id="myform" action="../php/" method="post">
+
+		<div class="form-group">
+			<label for="contactDemoName">Name <span class="text-danger">*</span></label>
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"></span>
+					<i class="fa fa-user" aria-hidden="true"></i>
+				</div>
+				<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label for="contactDemoEmail">Email <span class="text-danger">*</span></label>
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"></span>
+					<i class="fa fa-envelope" aria-hidden="true"></i>
+				</div>
+				<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label for="contactDemoSubject">Subject</label>
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"></span>
+					<i class="fa fa-pencil" aria-hidden="true"></i>
+				</div>
+				<input type="text" class="form-control" id="subject" name="subject"
+						 placeholder="Subject">
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label for="contactDemoMessage">Message <span class="text-danger">*</span></label>
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"></span>
+					<i class="fa fa-comment" aria-hidden="true"></i>
+				</div>
+				<textarea class="form-control" rows="5" id="message" name="message"
+							 placeholder="(2000 characters max)"></textarea>
+			</div>
+		</div>
+
+		<!-- reCAPTCHA -->
+		<div class="g-recaptcha" data-sitekey="6LepKsIUAAAAAKR4zR6LWH2iPLs7kYRaOdqd0jRE"></div>
+
+		<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+		<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+	</form>
+
+	<!--empty area for form error/success output-->
+	<div class="row">
+		<div class="col-xs-12">
+			<div id="output-area"></div>
+		</div>
+	</div>
+</div>
 </body>
 </html>
